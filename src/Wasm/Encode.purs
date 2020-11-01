6d1480c153b0287e05991822ec825f36afaac217
kritzcreek <christoph.hegemann1337@gmail.com>
2020-11-01 16:05:54 +0100

    Defines unsigned and signed leb128

--- src/Wasm/Encode.purs
+++ src/Wasm/Encode.purs
@@ -2,7 +2,6 @@ module Wasm.Encode where
 
 import Prelude
 
-import Effect.Console as Console
 import Data.Array as Array
 import Data.Foldable (sequence_)
 import Data.Int.Bits as Bits
@@ -11,6 +10,31 @@ import DynamicBuffer (DBuffer)
 import DynamicBuffer as DBuffer
 import Effect (Effect)
 import Effect.Exception (Error)
+import Partial.Unsafe (unsafeCrashWith)
+
+unsigned_leb128 :: DBuffer -> Int -> Effect Unit
+unsigned_leb128 b x =
+  if x < 0
+  then unsafeCrashWith "Negative unsigned_leb128"
+  else
+    let seven_bits = x `Bits.and` 0x7F in
+    let shifted = x `Bits.shr` 7 in
+    if shifted == 0
+    then DBuffer.add_int8 b seven_bits
+    else do
+      DBuffer.add_int8 b (seven_bits `Bits.or` 0x80)
+      unsigned_leb128 b shifted
+
+signed_leb128 :: DBuffer -> Int -> Effect Unit
+signed_leb128 b x =
+  let seven_bits = x `Bits.and` 0x7F in
+  let shifted = x `Bits.shr` 7 in
+  if (shifted == 0 && seven_bits `Bits.and` 0x40 == 0)
+     || (shifted == -1 && seven_bits `Bits.and` 0x40 /= 0)
+  then DBuffer.add_int8 b seven_bits
+  else do
+    DBuffer.add_int8 b (seven_bits `Bits.or` 0x80)
+    signed_leb128 b shifted
 
 write_header :: DBuffer -> Effect Unit
 write_header b = do
@@ -23,17 +47,8 @@ write_header b = do
   DBuffer.add_int8 b 0x00
   DBuffer.add_int8 b 0x00
 
-{-
-(module
-  (func 0 (param 0 i32) (result i32)
-    local.get 0
-    i32.const 10
-    i32.add)
-  (export "addten" (func 0)))
--}
-
-reserve_size_bytes :: DBuffer -> Effect Int
-reserve_size_bytes b = do
+reserve_section_size :: DBuffer -> Effect Int
+reserve_section_size b = do
   start <- DBuffer.get_position b
   DBuffer.add_int8 b 0
   DBuffer.add_int8 b 0
@@ -57,25 +72,11 @@ write_section_size b offset s = do
   DBuffer.set b (offset + 4) chunk5
 
 write_u32 :: DBuffer -> Int -> Effect Unit
-write_u32 b s = do
-  let size = s + 0
-  let chunk1 = (s `Bits.and` 0x7f) `Bits.or` 0x80
-  let chunk2 = ((Bits.shr s 7) `Bits.and` 0x7f) `Bits.or` 0x80
-  let chunk3 = ((Bits.shr s 14) `Bits.and` 0x7f) `Bits.or` 0x80
-  let chunk4 = ((Bits.shr s 21) `Bits.and` 0x7f) `Bits.or` 0x80
-  let chunk5 = (Bits.shr s 28) `Bits.and` 0x7f
-  DBuffer.add_int8 b chunk1
-  DBuffer.add_int8 b chunk2
-  DBuffer.add_int8 b chunk3
-  DBuffer.add_int8 b chunk4
-  DBuffer.add_int8 b chunk5
+write_u32 = unsigned_leb128
 
 write_vec :: DBuffer -> Array (Effect Unit) -> Effect Unit
 write_vec b elements = do
-  let length = Array.length elements
-  Console.log ("String length: " <> show length)
-  -- DBuffer.add_int8 b length
-  write_u32 b length
+  write_u32 b (Array.length elements)
   sequence_ elements
 
 write_custom_section :: DBuffer -> Effect Unit
@@ -85,10 +86,74 @@ write_custom_section b = do
 
 type SectionId = Int
 
+write_result_type :: DBuffer -> Effect Unit
+write_result_type b = write_vec b [ DBuffer.add_int8 b 0x7F ]
+
+write_func_type :: DBuffer -> Effect Unit
+write_func_type b = do
+  DBuffer.add_int8 b 0x60
+  write_result_type b
+  write_result_type b
+
+write_type_section :: DBuffer -> Effect Unit
+write_type_section b = write_section b 1 do
+  write_vec b [ write_func_type b ]
+
+write_function_section :: DBuffer -> Effect Unit
+write_function_section b = write_section b 3 do
+  write_vec b [ DBuffer.add_int8 b 0 ]
+
+write_export :: DBuffer -> Effect Unit
+write_export b = do
+  write_vec b
+    [ DBuffer.add_int8 b 0x61
+    , DBuffer.add_int8 b 0x64
+    , DBuffer.add_int8 b 0x64
+    ]
+  DBuffer.add_int8 b 0x00
+  DBuffer.add_int8 b 0x00
+
+write_export_section :: DBuffer -> Effect Unit
+write_export_section b = write_section b 7 do
+  write_vec b [ write_export b ]
+
+write_expr :: DBuffer -> Effect Unit
+write_expr b = do
+  -- local.get 0
+  DBuffer.add_int8 b 0x20
+  DBuffer.add_int8 b 0x00
+
+  -- i32.const 10
+  DBuffer.add_int8 b 0x41
+  signed_leb128 b (-16452)
+
+  -- i32.add
+  DBuffer.add_int8 b 0x6A
+
+  -- end
+  DBuffer.add_int8 b 0x0B
+
+write_func :: DBuffer -> Effect Unit
+write_func b = do
+  write_vec b [ DBuffer.add_int8 b 1 *> DBuffer.add_int8 b 0x7F ]
+  write_expr b
+
+write_code :: DBuffer -> Effect Unit
+write_code b = do
+  size_offset <- reserve_section_size b
+  start <- DBuffer.get_position b
+  write_func b
+  end <- DBuffer.get_position b
+  write_section_size b size_offset (end - start)
+
+write_code_section :: DBuffer -> Effect Unit
+write_code_section b = write_section b 10 do
+  write_vec b [ write_code b ]
+
 write_section :: DBuffer -> SectionId -> Effect Unit -> Effect Unit
 write_section b id f = do
   DBuffer.add_int8 b id
-  size_offset <- reserve_size_bytes b
+  size_offset <- reserve_section_size b
   start <- DBuffer.get_position b
   f
   end <- DBuffer.get_position b
@@ -101,4 +166,8 @@ write_module cb = do
   b <- DBuffer.create 1024
   write_header b
   write_custom_section b
+  write_type_section b
+  write_function_section b
+  write_export_section b
+  write_code_section b
   DBuffer.writeToFile "bytes.wasm" b cb
