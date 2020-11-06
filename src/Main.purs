019aad709fe28e75ee5bd1436680ee6bc846d0d7
kritzcreek <christoph.hegemann1337@gmail.com>
2020-11-06 13:12:30 +0100

    compiles a test module

--- src/Main.purs
+++ src/Main.purs
@@ -9,11 +9,20 @@ import Effect.Class.Console as Console
 import Wasm.Encode as Encode
 import Wasm.Syntax as Syntax
 
+testModule :: Syntax.Module
+testModule =
+  Syntax.emptyModule {
+    types = [ { arguments: [Syntax.I32, Syntax.I32], results: [Syntax.I32] } ],
+    funcs = [ { type: 0, locals: [], body: [ Syntax.LocalGet 0, Syntax.LocalGet 1, Syntax.I32Add ] } ],
+    exports = [ { name: "myadd", desc: Syntax.ExportFunc 0 }]
+  }
+
+
 foreign import runWasm :: String -> Effect Unit
 
 main :: Effect Unit
 main = do
-  buf <- Encode.write_module Syntax.emptyModule
+  buf <- Encode.write_module testModule
   DBuffer.writeToFile "bytes.wasm" buf case _ of
     Nothing ->
       runWasm "bytes.wasm"
