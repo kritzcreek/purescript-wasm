b26ea431ba7a72ebf8afe51c3c5ae9ee95975775
Christoph Hegemann <christoph@deepchannel.com>
2023-10-26 17:57:58 +0200

    removes debug code

--- example/src/Driver.purs
+++ example/src/Driver.purs
@@ -5,7 +5,6 @@ import Prelude
 import Compiler as Compiler
 import Data.ArrayBuffer.Types (Uint8Array)
 import Data.Either (Either(..))
-import Debug (spyWith)
 import Parser as Parser
 import Partial.Unsafe (unsafeCrashWith)
 import Printer as Printer
@@ -23,7 +22,7 @@ compileProgram input = do
         Right typed -> do
           let { result } = Rename.renameProgram typed
           let compiled = Compiler.compileProgram result
-          Encode.encodeModule (spyWith "Compiled" show compiled)
+          Encode.encodeModule compiled
 
 renameProgram :: String -> String
 renameProgram input =
